1e565d5a9f4faeee89e3ae15df3da91b74e28643
guptaprakhariitr <prakharpguptag@gmail.com>
2020-08-16 19:19:14 +0530

    Navigation setup

--- app/src/main/java/com/example/getthetrack/MainActivity.java
+++ app/src/main/java/com/example/getthetrack/MainActivity.java
@@ -39,5 +39,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.loginButton);
+        login.setEnabled(false);
+        signup = findViewById(R.id.signupbut);
+        login.setBackgroundColor(Color.rgb(255,255,255));
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
+        additional = findViewById(R.id.additionalInfo);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            Intent myIntent = new Intent(MainActivity.this, homeActivity.class);
+            MainActivity.this.startActivity(myIntent);
+            finish();
+        }
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, signup.class);
+                Log.i("iiop","iio");
+                startActivity(intent);
+                Log.i("iio","iio");
+            }
+        });
+        email.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                login.setEnabled(true);
+                login.setBackgroundColor(Color.rgb(28,130,211));
+                return false;
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals("") ){
+                    email.setError("This Field is required");
+                }
+                else if(pass.getText().toString().equals("")){
+                    pass.setError("This Field is required");
+                }
+                else{
+                Signin();
+                login.setEnabled(false);
+                    login.setBackgroundColor(Color.rgb(255,255,255));
+                }
+            }
+        });
+
+    }
+    void Signin(){
+        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent myIntent = new Intent(MainActivity.this, homeActivity.class);
+                            MainActivity.this.startActivity(myIntent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            additional.setText("NOTE : If you are logging in for the first time use your phone number as password. \nIf still not able to login Check if You Are Registered.");
+                            login.setEnabled(true);
+                            login.setBackgroundColor(Color.rgb(28,130,211));
+                            pass.setText("");
+                        }
+                        // ...
+                    }
+                });
     }
 }
